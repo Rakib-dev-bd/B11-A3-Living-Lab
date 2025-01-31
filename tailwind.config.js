e31c64be14dee75e9354698c883ae7b17113db55
Md. Rakib Islam <r.s.rakib.business@gmail.com>
2025-01-31 14:20:59 +0600

    Added some section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#3490dc",
-        secondary: "#ffed4a",
-        danger: "#e3342f",
-      },
-      spacing: {
-        128: "32rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
-  },
-  plugins: [require("daisyui")],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#3490dc",
+//         secondary: "#ffed4a",
+//         danger: "#e3342f",
+//       },
+//       spacing: {
+//         128: "32rem",
+//       },
+//       borderRadius: {
+//         "4xl": "2rem",
+//       },
+//     },
+//   },
+//   plugins: [require("daisyui")],
+// };
